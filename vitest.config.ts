960d6e5a98a8a925b77c8aa6d132d064667fa19e
luw2007 <luw2007@gmail.com>
2026-09-10 23:27:56 +0800

    test: allow integration startup on CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     fileParallelism: false,
+    testTimeout: 30_000,
     include: ['tests/**/*.test.{js,ts,tsx}'],
     setupFiles: ['./tests/setup/vitest.setup.ts'],
     coverage: {
